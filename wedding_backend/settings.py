3f424f3a5992879114461d4fce7e837432f4025b
toan.tranduc <toan.tranduc@vti.com.vn>
2023-06-02 13:20:22 +0700

    a

--- wedding_backend/settings.py
+++ wedding_backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'api',
-    'templates.controller'
 ]
 
 MIDDLEWARE = [
@@ -122,9 +121,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'templates', 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
